cd764bc374b246f15fbdd5cfd35818b41967a6fd
Akshay-Durgade <113453627+Akshay-Durgade@users.noreply.github.com>
2022-09-24 20:27:11 +0530

    Add files via upload

---
+++ Assignment 2/Sheet 1/P10.java
@@ -0,0 +1,18 @@
+class P10
+{
+	public static void main(String args[])
+	{
+		for(int i=69;i>=65;i--)
+		{
+			for(int j=i;j>65;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j<=69;j++)
+			{
+				System.out.print((char)j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
